0bc0a23ccf81e6c915ae7bd98db2c6f29a57af66
fenderz <federicopfund@gmail.com>
2021-11-22 21:21:15 -0300

    controller

--- routes/web.php
+++ routes/web.php
@@ -17,8 +17,8 @@ Route::get('/',function(){
     return view("home.home");
 })-> name("dashboard");
 
-Route::prefix('dashboars')->group(function(){
-    Route::get("/","DeshboaardController@index");
+Route::prefix('dashboard')->group(function(){
+   Route::get("/","DashboardController@index");
 });//ruta de tipo recurso:se le especifica un nombre y una ruta de la cual ase uso de un contralador.
 
-Route::resource("Abaut","AbaoutController");
+Route::resource("Abaut","AboutController");
