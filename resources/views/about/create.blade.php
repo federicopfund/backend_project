68489be21014a5ded1883c037ce45b8ba56f433e
fenderz <federicopfund@gmail.com>
2021-11-24 00:44:07 -0300

    token de validacion de formulario, encrypta

--- resources/views/about/create.blade.php
+++ resources/views/about/create.blade.php
@@ -5,7 +5,8 @@
     class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
     <h1 class="h2"{{ "About Create" }}</h1>
 </div>
-<form action="{{ route(" about.store ")}}" method="post">
+<form action="{{ route("About.store")}}" method="post">
+    {{ csrf_field() }}
     <div class="mb-3">
         <label for="name" class="form-label">Nombre</label>
         <input type="text" id="name" class="form-control" placeholder="name">
